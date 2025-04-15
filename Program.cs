582a111f8f9cbbeadfbbab5264bec509bc56afb1
AJ Cunha <argenisjc@hotmail.com>
2025-04-15 17:59:11 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace wk1ex1Review
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Message myMessage = new Message();
+
+            myMessage.DisplayMessage("\"Welcome to ITSE102\"");
+
+            Console.Read();
+        }
+    }
+    class Message
+    {
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
